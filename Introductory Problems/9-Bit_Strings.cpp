b674b06f474e97d29fcbeda2f7e9b29f41db3f22
Divya Musahib <divyamusahib@gmail.com>
2025-11-25 22:46:31 +0530

    added introductory problems

--- Introductory Problems/9-Bit_Strings.cpp
+++ Introductory Problems/9-Bit_Strings.cpp
@@ -24,3 +24,34 @@ int main() {
     cin >> n;
     cout << modExp(2, n);
 }
+
+// https://cses.fi/paste/9776fe0407a24a2feaf4de/
+// method 2 (no recursive stack space is getting used in this solution , it is iterative)
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll mod = 1e9 + 7;
+
+ll modExp(ll base, ll exp) {
+    ll ans = 1;
+    base %= mod;
+    // eg : to find = 5 ^ 13 
+    //              13 = 1101 (Binary)
+    //              5 ^ (8 + 4 + 1) 
+
+    while(exp > 0) {
+        // If current bit of exponent is 1, multiply result
+        if(exp & 1) {
+            ans = (ans * base) % mod; 
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return ans;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    cout << modExp(2, n);
+}
